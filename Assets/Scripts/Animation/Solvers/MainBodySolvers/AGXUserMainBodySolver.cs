0a295b21e77a930e45e26b0bd6310313e3c472b9
Nicholas Bindels <nc282705@dal.ca>
2026-07-15 15:59:23 -0300

    Added MainBodySolver for use with AGX

---
+++ Assets/Scripts/Animation/Solvers/MainBodySolvers/AGXUserMainBodySolver.cs
@@ -0,0 +1,133 @@
+using AnimationDataStructs;
+using UnityEngine;
+using MotionDataPacketClass;
+
+public class AGXUserMainBodySolver : MainBodySolverAbstract {
+    private float fixedTimeStep;
+
+    Vector3 targetPosition;
+    Quaternion targetRotation;
+
+
+
+    //--Body Roll Tail State
+    WhaleState currentState;
+    
+    private float rootTurnSpeed = 150f;
+
+    private float headTurnSpeed = 1f;
+
+    private float followRotationSpeed = 8f;
+
+    private float responseAtHead = 1f;
+
+    private float responseAtTail = 0.3f;
+
+
+    public AGXUserMainBodySolver(float fixedTimeStepIn, WhaleState startState){
+        fixedTimeStep = fixedTimeStepIn;
+        this.targetRotation = startState.Root.Rotation;
+        currentState = startState;
+    }
+
+    // combines filtered classic main body solver with body roll solver for better motion 
+    public override Global_AnimationData solveMainBody(MotionDataPacket currentPacket, Global_AnimationData previousState){
+        Global_AnimationData solvedRoot = classicMainBodySolve(currentPacket, previousState);
+        currentState = solveBodyRoll(solvedRoot);
+        return currentState.Root;
+    }
+    
+
+    // saves the internal position and travel distance for the body roll solver to use
+    private Global_AnimationData classicMainBodySolve ( MotionDataPacket currentPacket, Global_AnimationData previousState){
+        Global_AnimationData newState = new Global_AnimationData();
+
+        targetRotation = Quaternion.Euler(currentPacket.pitch, currentPacket.head, currentPacket.roll);
+
+        // Lerp and Slerp toward filtered targets
+        newState.Speed = currentPacket.speed;
+        newState.Rotation = Quaternion.Slerp(previousState.Rotation, targetRotation, fixedTimeStep * 0.5f);
+
+      
+        return newState;
+    }
+
+    //getters for outputting the new body roll state to the whale model
+    public LocalRotation_AnimationData[] getBodyState(){
+        return currentState.BodyLength;
+    }
+    public LocalRotation_AnimationData getHeadState(){
+        return currentState.Head;
+    }
+    // body roll solver
+    private WhaleState solveBodyRoll(Global_AnimationData solvedRoot)
+    {
+        // create a new state based on the current state
+        WhaleState newState = new WhaleState(currentState);
+        WhaleState previousState = currentState;
+        
+        Quaternion desiredWorldHeading = solvedRoot.Rotation;
+
+      
+        Quaternion rootRotationNow = Quaternion.Slerp(
+            previousState.Root.Rotation, 
+            desiredWorldHeading, 
+            rootTurnSpeed * fixedTimeStep
+        );
+
+    
+    
+        newState.Root = new Global_AnimationData
+        {
+            Position = new Vector3(0,0,0), 
+            Rotation = rootRotationNow
+        };
+    
+        Quaternion headWorldPrev = previousState.Root.Rotation *  previousState.Head.Rotation;
+      
+        Quaternion headWorldNow = Quaternion.Slerp(
+            headWorldPrev, 
+            desiredWorldHeading, 
+            headTurnSpeed * fixedTimeStep
+        );
+        
+        Quaternion headLocalNow = Quaternion.Inverse(newState.Root.Rotation) * headWorldNow;
+
+       
+        newState.Head = new LocalRotation_AnimationData { Rotation = headLocalNow };
+
+        newState.BodyLength = new LocalRotation_AnimationData[previousState.BodyLength.Length];
+
+        for (int i = 0; i < newState.BodyLength.Length; i++)
+        {
+            newState.BodyLength[i] = new LocalRotation_AnimationData { Rotation = previousState.BodyLength[i].Rotation };
+        }
+
+        Quaternion parentWorldPrev = headWorldPrev;
+        Quaternion parentWorldNow = headWorldNow;
+
+
+        // minus 8 to not roll the tail segments
+        int n = previousState.BodyLength.Length - 8;
+        for (int i = 0; i < n; i++)
+        {
+            float t = n > 1 ? (float)i / (n - 1) : 0f;
+            float rotationCurve = Mathf.Lerp(responseAtHead, responseAtTail, t);
+
+            Quaternion segWorldPrev = parentWorldPrev * previousState.BodyLength[i].Rotation;
+            Quaternion segWorldNow = Quaternion.Slerp(
+                segWorldPrev, parentWorldNow, followRotationSpeed * rotationCurve * fixedTimeStep
+            );
+
+            Quaternion segLocalNow = Quaternion.Inverse(parentWorldNow) * segWorldNow;
+
+
+            newState.BodyLength[i] = new LocalRotation_AnimationData { Rotation = segLocalNow };
+
+            parentWorldPrev = segWorldPrev;
+            parentWorldNow = segWorldNow;
+        }
+
+        return newState;
+    }
+}
