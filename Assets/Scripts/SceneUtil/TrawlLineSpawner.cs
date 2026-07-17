f2abbb2147aea711b3aa347a472ccf4c6a5ace03
unknown <danydiab04@gmail.com>
2026-07-17 18:05:39 -0300

    updated trawl line spawn

--- Assets/Scripts/SceneUtil/TrawlLineSpawner.cs
+++ Assets/Scripts/SceneUtil/TrawlLineSpawner.cs
@@ -1,5 +1,6 @@
 using AGXUnity;
 using AGXUnity.Rendering;
+using AGXUnity.Utils;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,11 +11,11 @@ public class TrawlLineSpawner : ScriptComponent{
     [SerializeField] Material wireMat;
 
     int minOnTrawl = 5;
-    int maxOnTrawl = 15;
+    int maxOnTrawl = 8;
 
     [SerializeField] float spacingDelta;
 
-    [SerializeField] float depth = 100;
+    [SerializeField] float depth;
 
     protected override bool Initialize(){
         routeWire();
@@ -23,6 +24,15 @@ public class TrawlLineSpawner : ScriptComponent{
     }
 
     public void routeWire(){
+        AGXUnity.RigidBody buoyRB = buoy.GetComponent<AGXUnity.RigidBody>().GetInitialized<AGXUnity.RigidBody>();
+
+        Vector3 buoyCurrPos = buoy.transform.position;
+        buoyCurrPos.y = depth;
+
+        agx.Vec3 buoyPos = buoyCurrPos.ToHandedVec3();
+
+        buoyRB.Native.setPosition(buoyPos);
+
         Wire wire = gameObject.AddComponent<Wire>();
         WireRenderer renderer = gameObject.AddComponent<WireRenderer>();
 
@@ -30,24 +40,30 @@ public class TrawlLineSpawner : ScriptComponent{
         
         wire.Diameter = .1f;
         
-        WireRouteNode buoyRouteNode = WireRouteNode.Create(Wire.NodeType.BodyFixedNode,buoy,Vector3.up * -2.12f, Quaternion.identity);
+        float buoyHeight = buoy.GetComponentInChildren<AGXUnity.Collide.Capsule>().Height;
+
+        WireRouteNode buoyRouteNode = WireRouteNode.Create(Wire.NodeType.BodyFixedNode,buoy,Vector3.up * -buoyHeight, Quaternion.identity);
         wire.Route.Add(buoyRouteNode);
 
 
-        int randT = Random.Range(0,1);
+        float randT = Random.Range(0.0f,1.0f);
         int numTraps = Mathf.RoundToInt(Mathf.Lerp(minOnTrawl,maxOnTrawl, randT));
+        
+        
+        float trapHeight = lobsterTrap.GetComponentInChildren<AGXUnity.Collide.Box>().HalfExtents.y;
+
 
         for(int i = 0; i < numTraps; i++){
             float deltaOffset = spacingDelta * i;
             
-            Vector3 offset = new Vector3(deltaOffset,-depth,0);
-            Vector3 position = offset + buoy.transform.position;
+            Vector3 offset = new Vector3(deltaOffset, -depth, 0);
+            Vector3 position = buoyRB.Native.getPosition().ToHandedVector3() + offset;
 
             GameObject trap = Instantiate(lobsterTrap,position,Quaternion.identity,gameObject.transform);
 
             Wire.NodeType nodeType = i == numTraps - 1 ? Wire.NodeType.BodyFixedNode : Wire.NodeType.EyeNode;
 
-            WireRouteNode trapRouteNode = WireRouteNode.Create(nodeType,trap,Vector3.zero, Quaternion.identity);
+            WireRouteNode trapRouteNode = WireRouteNode.Create(nodeType,trap,(Vector3.up * trapHeight / 2), Quaternion.identity);
             wire.Route.Add(trapRouteNode);
 
         }
